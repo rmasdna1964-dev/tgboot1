36290a9fb6142c7e869fbd8553cfec7d0ce8ca34
rmasdna1964-dev <rmatygina1964@gmail.com>
2026-09-07 20:50:01 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, C
 from aiogram.fsm.storage.memory import MemoryStorage
 
 # Настройки и токен
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8872260684:AAHEhMfCuLTfG0RK1kjmqUmDS-TXRiQUWzk").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8872260684:AAED-oo-qBqge-nTot8Kva1H4wxjRZvSHSM").strip()
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(storage=MemoryStorage())
